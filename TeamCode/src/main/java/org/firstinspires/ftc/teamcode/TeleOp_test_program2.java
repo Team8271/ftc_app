d97ffdcaf27c97898a6aa605e6cf8ec0445af927
Team8271 <31601375+Team8271@users.noreply.github.com>
2018-11-13 11:07:16 -0700

    clean up wrong team

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_test_program2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_test_program2.java
@@ -29,17 +29,13 @@ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
 TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*//*
-
+*/
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-*/
 /**
  * This file contains an minimal example of a Linear Tele "OpMode".
  *
@@ -48,72 +44,45 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  *
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- *//*
+ */
 
-
-@TeleOp(name="Example: EriksCollector", group="Examples")  // @Autonomous(...) is the other common choice
+@TeleOp(name="test: TeleOp", group="Examples")  // @Autonomous(...) is the other common choice
 //@Disabled
-public class EriksCollector extends LinearOpMode {
-
-    */
-/* Declare OpMode members. *//*
-
+public class TeleOp_test_program2 extends LinearOpMode { /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
+
+    Our_HardwareSetup robot      = new Our_HardwareSetup();
+
     //motors
-    //DcMotor motorLeft = null;
-    DcMotor motorArm2 = null;
-    DcMotor motorArm1 = null;
 
-    //servos
-    //Servo servoHandL = null;
-   //Servo servoHandR = null;
-
-    //colector
-    Servo servo1 = null;
-
-    //Create and set default hand positions variables. To be determined based on your build
-    double counterclockwise = 0.2;
-    double clockwise = 0.8;
-    double stop = 0.5;
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        robot.init(hardwareMap);
+
+
         //adds feedback telemetry to DS
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        */
-/* eg: Initialize the hardware variables. Note that the strings used here as parameters
+        /* eg: Initialize the hardware variables. Note that the strings used here as parameters
          * to 'get' must correspond to the names assigned during the robot configuration
          * step (using the FTC Robot Controller app on the phone).
-         *//*
-
-         motorLeft  = hardwareMap.dcMotor.get("motorL");
-         motorRight = hardwareMap.dcMotor.get("motorR");
-         motorArm2 = hardwareMap.dcMotor.get("motorArm");
-        motorArm1 = hardwareMap.dcMotor.get("motorArm");
-         servoHandL = hardwareMap.servo.get("servoHandL"); //assuming a pushBot configuration of two servo grippers
-         servo1 = hardwareMap.servo.get("servo1");
+         */
 
         // eg: Set the drive motor directions:
-         "Reverse" the motor that runs backwards when connected directly to the battery
-         motorLeft.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-         motorRight.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
-         motorArm.setDirection(DcMotor.Direction.FORWARD); // Can change based on motor configuration
+        // "Reverse" the motor that runs backwards when connected directly to the battery
+         // Set to FORWARD if using AndyMark motors
 
-        //Set servo hand grippers to open position.
-        // servoHandL.setPosition(OPEN);
-         //servoHandR.setPosition(OPEN);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
 
-        */
-/************************
+        /************************
          * TeleOp Code Below://
-         *************************//*
-
+         *************************/
 
         while (opModeIsActive()) {  // run until the end of the match (driver presses STOP)
             telemetry.addData("Status", "Run Time: " + runtime.toString());
@@ -121,40 +90,42 @@ public class EriksCollector extends LinearOpMode {
 
             // tank drive set to gamepad1 joysticks
             //(note: The joystick goes negative when pushed forwards)
-          //  motorLeft.setPower(gamepad1.left_stick_y);
-          //  motorRight.setPower(gamepad1.right_stick_y);
-*/
-/*
+            robot.motorLeft.setPower(gamepad1.left_stick_y /4);
+            robot.motorRight.setPower(gamepad1.right_stick_y /4);
+
             // Arm Control - Uses dual buttons to control motor direction
-            if(gamepad1.right_bumper)
+            if(gamepad2.right_bumper)
             {
-                motorArm.setPower(-gamepad1.right_trigger); // if both Bumper + Trigger, then negative power, runs arm down
+                robot.motorArm.setPower(-gamepad2.right_trigger); // if both Bumper + Trigger, then negative power, runs arm down
+                //read current position
             }
-           else
+            else if (!gamepad2.right_bumper)
             {
-                motorArm.setPower(gamepad1.right_trigger);  // else trigger positive value, runs arm up
+                robot.motorArm.setPower(gamepad2.right_trigger);  // else trigger positive value, runs arm up
+                //read current position
             }
-        *//*
+            else
+            {
+                //hold motor position
+            }
 
             //servo commands
             if(gamepad2.a) //button 'a' will open
             {
-                servo1.setPosition(clockwise);
-
+                robot.servoHandR.setPosition(robot.OPEN);
             }
             else if (gamepad2.b) //button 'b' will close
             {
-                servo1.setPosition(counterclockwise);
+                robot.servoHandR.setPosition(robot.OPEN);
+            }
 
-            }
-            else if (gamepad2.x)
-            {
-                servo1.setPosition(stop);
-            }
+
+
+
+
 
 
             idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop
         }
     }
 }
-*/
